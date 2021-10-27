6f6e1331dffaf13735dd4324cb887815b00e6131
Matvei Mochalov <tutaitam1@gmail.com>
2021-10-27 23:38:39 +0200

    Create 6.cpp

---
+++ Введение/Условный оператор/6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int X, Y, X2, Y2;
+    cin >> X >> Y >> X2 >> Y2;
+   if (X==X2 || Y==Y2 ) {
+      cout << "YES";
+    } else{
+      cout << "NO";
+    }
+    
+    
+  return 0;
+}
