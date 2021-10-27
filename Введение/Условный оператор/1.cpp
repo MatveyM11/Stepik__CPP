34be66c9f590b2dcdd5222d285db9b095b6af285
Matvei Mochalov <tutaitam1@gmail.com>
2021-10-27 22:06:11 +0200

    Create 1.cpp

---
+++ Введение/Условный оператор/1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int X, Y;
+    cin >> X >> Y;
+    if (X>=Y){
+    cout << X;
+    }else{
+    cout << Y;  
+    }
+    
+    
+  return 0;
+}
