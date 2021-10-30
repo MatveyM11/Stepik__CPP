44182722d74dbad663040750d9024ab07c9c831a
Matvei Mochalov <tutaitam1@gmail.com>
2021-10-30 18:25:27 +0200

    Create 9.cpp

---
+++ Введение/Условный оператор/9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int X, Y, X2, Y2;
+    cin >> X >> Y >> X2 >> Y2;
+   if (X==X2 || Y==Y2 ||(X!=X2 && Y!=Y2 && (X-X2==Y-Y2 ||X-X2+Y-Y2==0  ))) {
+      cout << "YES";
+    } else{
+      cout << "NO";
+    }
+    
+    
+  return 0;
+}
