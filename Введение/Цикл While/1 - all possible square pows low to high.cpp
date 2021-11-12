da48d174b1ac0b0f434af9162328047f075687ef
Matvei Mochalov <tutaitam1@gmail.com>
2021-11-12 23:55:23 +0100

    Create 1 - all possible square pows low to high.cpp
    По данному целому числу N распечатайте все квадраты натуральных чисел, не превосходящие N, в порядке возрастания.
    
    Формат входных данных
    Вводится натуральное число.
    
    Формат выходных данных
    Выведите ответ на задачу.
    
    Sample Input:
    
    50
    Sample Output:
    
    1 4 9 16 25 36 49

---
+++ Введение/Цикл While/1 - all possible square pows low to high.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int N;
+ int i = 1; //counter
+ int sqr;
+ cin >> N;
+    
+ 
+    while(i*i <= N){
+            sqr=+ i*i;
+            cout << sqr << " ";
+            i++;  
+            
+        
+    }
+}
