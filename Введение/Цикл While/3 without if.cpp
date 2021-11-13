99f796ea1bb80dcb9595166b78df9aa9bf42325f
Matvei Mochalov <tutaitam1@gmail.com>
2021-11-13 15:57:13 +0100

    Create 3 without if.cpp
    По данному числу N распечатайте все целые степени двойки, не превосходящие N, в порядке возрастания.
    
    Формат входных данных
    Вводится натуральное число.
    
    Формат выходных данных
    Выведите ответ на задачу.
    
    Sample Input:
    
    50
    Sample Output:
    
    1 2 4 8 16 32

---
+++ Введение/Цикл While/3 without if.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int N;
+int X = 1;
+
+cin >> N;
+
+    while(X <= N){
+        
+        cout << X << " ";
+        
+        X = X * 2;
+        
+    }
+
+
+    return 0;
+}
