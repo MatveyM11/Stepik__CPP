0b1be9ce656436e4f117a89aa39c337dd2dd384c
Matvei Mochalov <tutaitam1@gmail.com>
2021-11-13 19:59:21 +0100

    Create 7.cpp
    Последовательность состоит из натуральных чисел и завершается числом 0. Определите значение наибольшего элемента последовательности.
    
    Формат входных данных
    Вводится последовательность целых чисел, оканчивающаяся числом 0 (само число 0 в последовательность не входит, а служит как признак ее окончания).
    Формат выходных данных
    Выведите ответ на задачу.
    Sample Input:
    
    1
    7
    9
    0
    Sample Output:
    
    9

---
+++ Введение/Цикл While/7.cpp
@@ -0,0 +1,28 @@
+  
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int N = 1;
+int i = 0;//counter
+
+
+    while(N != 0){
+     
+     cin >> N;
+     
+     if(i >= N){
+         i =+ i;  
+     }
+     else{
+         i =+ N;
+     }
+   
+     }
+     
+     cout << i;
+           
+    return 0;
+}
