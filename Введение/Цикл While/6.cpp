3572c80cf94dd07c8d6f0d50c1cd25f3dd210a27
Matvei Mochalov <tutaitam1@gmail.com>
2021-11-13 19:32:25 +0100

    Create 6.cpp
    Определите сумму всех элементов последовательности, завершающейся числом 0.
    
    Формат входных данных
    Вводится последовательность целых чисел, оканчивающаяся числом 0 (само число 0 в последовательность не входит, а служит как признак ее окончания.
    Формат выходных данных
    Выведите ответ на задачу.
    Sample Input:
    
    3
    6
    8
    0
    Sample Output:
    
    17

---
+++ Введение/Цикл While/6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int N = 1;
+int i = 0;//counter
+
+
+    while(N != 0){
+     
+     cin >> N;
+     i += N;
+     }
+    
+     cout << i;
+           
+    return 0;
+}
