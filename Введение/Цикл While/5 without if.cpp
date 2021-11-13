acbe95ab89715d1b66d2c84f3688d27cb67aca67
Matvei Mochalov <tutaitam1@gmail.com>
2021-11-13 17:52:40 +0100

    Create 5 without if.cpp
    Программа получает на вход последовательность целых неотрицательных чисел, каждое число записано в отдельной строке. Последовательность завершается числом 0, при считывании которого программа должна закончить свою работу и вывести количество членов последовательности (не считая завершающего числа 0).
    Числа, следующие за числом 0, считывать не нужно.
    
    Формат входных данных
    Вводится последовательность целых чисел, заканчивающаяся числом 0.
    Формат выходных данных
    Выведите ответ на задачу.
    Sample Input:
    
    1
    7
    9
    0
    5
    Sample Output:
    
    3

---
+++ Введение/Цикл While/5 without if.cpp
@@ -0,0 +1,22 @@
+  
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int N;
+int i = 0;//counter
+
+    cin >> N;
+
+    while(N != 0){
+     
+     cin >> N;
+     i++;
+     }
+     
+     cout << i;
+           
+    return 0;
+}
