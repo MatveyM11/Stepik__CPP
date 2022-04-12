393e06ee2cd54c1a6bdab864ab7e057729c7f994
Matvey Mochalov <tutaitam1@gmail.com>
2022-04-12 03:33:09 +0200

    Create 3.cpp

---
+++ Введение/Строки и символы/3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    char a;
+    cin >> a;
+    if(a >= 97 and a <= 122){
+          a =+ a-32;
+    }
+    else if(a >= 65 and a <= 90){
+        a =+ a+32;
+    }
+    cout << a;
+   
+
+    return 0;
+}
